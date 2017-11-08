9d277f321ed58b2ee468e8b0ca091484bc59e0ff
LiZonglun <Mrzonglunli@gmail.com>
2017-11-08 10:22:59 +0800

    add BinaryTree.java

--- src/me/zonglun/BinaryTree/BinarySearch2.java
+++ src/me/zonglun/BinaryTree/BinarySearch2.java
@@ -8,6 +8,8 @@
 
 package me.zonglun.BinaryTree;
 
+import me.zonglun.Sort.InsertionSort;
+
 /**
  * 〈一句话功能简述〉<br>
  * 〈递归版本的二分查找算法〉
@@ -21,12 +23,24 @@ public class BinarySearch2 {
     }
 
     private static int find(Comparable[] arr, int l, int r, Comparable target) {
-        if (l < r)
+        if (l > r)
             return -1;
         int mid = l + (r - l) / 2;
         if (arr[mid].compareTo(target) == 0)
             return mid;
         else if (arr[mid].compareTo(target) < 0)
-            return find(arr,)
+            return find(arr, mid + 1, r, target);
+        else
+            return find(arr, l, mid - 1, target);
+    }
+
+    public static int find(Comparable[] arr, Comparable target) {
+        return find(arr, 0, arr.length - 1, target);
+    }
+
+    public static void main(String[] args) {
+        Comparable[] arr = {4, 6, 2, 1, 7, 12, 78, 94, 123, 11};
+        InsertionSort.sort(arr);
+        System.out.println(BinarySearch2.find(arr, 11));
     }
 }
