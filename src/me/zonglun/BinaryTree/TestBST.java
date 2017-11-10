79fdc4747fa489a831a7d7564c8f18700a600e77
LiZonglun <Mrzonglunli@gmail.com>
2017-11-10 15:38:52 +0800

    update TestBST

--- src/me/zonglun/BinaryTree/TestBST.java
+++ src/me/zonglun/BinaryTree/TestBST.java
@@ -22,7 +22,7 @@ public class TestBST {
         // 取n个取值范围在[0...m)的随机整数放进二分搜索树中
         int N = 10;
         int M = 100;
-        for(int i = 0 ; i < N ; i ++){
+        for (int i = 0; i < N; i++) {
             Integer key = (int) (Math.random() * M);
             // 为了后续测试方便,这里value值取和key值一样
             bst.insert(key, key);
@@ -53,5 +53,31 @@ public class TestBST {
         System.out.println("levelOrder: ");
         bst.levelOrder();
         System.out.println();
+
+        // 测试 removeMin
+        // 输出的元素应该是从小到大排列的
+        System.out.println("Test removeMin: ");
+        while (!bst.isEmpty()) {
+            System.out.print("min: " + bst.minimum() + " , ");
+            bst.removeMin();
+            System.out.println("After removeMin, size = " + bst.size());
+        }
+        System.out.println();
+
+        for (int i = 0; i < N; i++) {
+            Integer key = (int) (Math.random() * M);
+            // 为了后续测试方便,这里value值取和key值一样
+            bst.insert(key, key);
+        }
+        // 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
+
+        // 测试 removeMax
+        // 输出的元素应该是从大到小排列的
+        System.out.println("Test removeMax: ");
+        while (!bst.isEmpty()) {
+            System.out.print("max: " + bst.maximum() + " , ");
+            bst.removeMax();
+            System.out.println("After removeMax, size = " + bst.size());
+        }
     }
 }
