346f339c8393df7c1f69c717c7a67745203de13d
LiZonglun <Mrzonglunli@gmail.com>
2017-11-18 17:00:38 +0800

    update offer

--- src/me/zonglun/SwordToOffer/PrintListFromTailToHead.java
+++ src/me/zonglun/SwordToOffer/PrintListFromTailToHead.java
@@ -9,13 +9,15 @@
 package me.zonglun.SwordToOffer;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * 〈一句话功能简述〉<br>
  * 〈输入一个链表，从尾到头打印链表每个节点的值。〉
  *@subject 输入一个链表，从尾到头打印链表每个节点的值
  * @Strategies
- *           思路：
+ *           思路：1. 可以借用栈的后进先出来实现 从尾到头的打印
+ *           思路2： 使用递归来实现
  * @author Administrator
  * @create 2017/11/16 0016
  * @since 1.0.0
@@ -31,7 +33,26 @@ public class PrintListFromTailToHead {
     }
 
     public ArrayList<Integer> printList(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;   // 维护指针指向后一个
+        }
 
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!stack.empty())
+            list.add(stack.pop());
+        return list;
+    }
+
+
+    private ArrayList<Integer> list = new ArrayList<>();
+    public ArrayList<Integer> printList2(ListNode listNode) {
+        if (listNode != null) {
+            this.printList2(listNode.next);
+            list.add(listNode.val);
+        }
+        return list;
     }
 
 }
