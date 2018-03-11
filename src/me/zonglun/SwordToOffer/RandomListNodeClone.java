132122a6396f51123d07f0a09c18d4c1270bed67
zonglunLi <mrzonglunli@gmail.com>
2018-03-11 14:00:44 +0800

    update offer for Java

--- src/me/zonglun/SwordToOffer/RandomListNodeClone.java
+++ src/me/zonglun/SwordToOffer/RandomListNodeClone.java
@@ -47,26 +47,26 @@
              current = cloneNode.next;
          }
 
-         RandomListNode current2 = pHead;
-         // 第二步 复制random 指针  current2 是原来链表的结点 pCur.next是复制pCur的结点
-         while (current2 != null) {
-             if (current2.random != null) {
-                 current2.next.random = current2.random;
+         current = pHead;       // 重新使得现在指针指向头节点
+         // 第二步 复制random 指针  current 是原来链表的结点 pCur.next是复制pCur的结点
+         while (current != null) {
+             if (current.random != null) {
+                 current.next.random = current.random.next;
              }
-             current2 = current2.next.next; // 维护指针后移
+             current = current.next.next; // 维护指针后移
          }
 
-         // todo 第三步 把这个长链表拆分成两个链表：把奇数节点链接起来为原始节点，偶数节点链接起来为克隆节点
-         RandomListNode current3 = pHead;
-         RandomListNode cloneHead = current3.next;
+         // 第三步 把这个长链表拆分成两个链表：把奇数节点链接起来为原始节点，偶数节点链接起来为克隆节点
+         current = pHead;       // 重新使得现在指针指向头节点
+         RandomListNode cloneHead = pHead.next;
          RandomListNode cloneNode = cloneHead;
-         while (cloneNode != null) {
-             cloneNode.next = current3.next.next;   // 此步使得 A'->B'->C'
-             if (current3.next != null) {
-                 current3.next = current3.next.next;
+         while (current != null) {
+             current.next = current.next.next;   // 此步使得 A'->B'->C'
+             if (cloneNode.next != null) {
+                 cloneNode.next = cloneNode.next.next;
              }
              // 上两步分别完成了两条链表，两个节点的指针指向，后面是使得其循环实现
-             current3 = current3.next;
+             current = current.next;
              cloneNode = cloneNode.next;
          }
          return cloneHead;
