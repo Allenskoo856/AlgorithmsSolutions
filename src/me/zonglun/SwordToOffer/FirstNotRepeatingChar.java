63b9500a31591c6472fd3b875adbdbfebb7f4b40
ZongLUnli <mrzonglunli@gmail.com>
2018-04-05 20:55:23 +0800

    some changes

---
+++ src/me/zonglun/SwordToOffer/FirstNotRepeatingChar.java
@@ -0,0 +1,24 @@
+package me.zonglun.SwordToOffer;
+
+import java.util.LinkedHashMap;
+
+/*
+* @subejct: 在一个字符串(1<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置
+* @example: 例如输入 “abaccdeff" 则输出”b“
+* */
+public class FirstNotRepeatingChar {
+
+    public int solution(String str) {
+        char[] chars = str.toCharArray();
+        int[] map = new int[256];
+        for (int i = 0; i < chars.length; i++) {
+            map[chars[i]]++;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (map[chars[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/me/zonglun/SwordToOffer/InversePairs.java
@@ -0,0 +1,20 @@
+package me.zonglun.SwordToOffer;
+
+/**
+ * @author : Administrator
+ * @subject : 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+ *              输入一个数组,求出这个数组中的逆序对的总数P。
+ *              并将P对 1000000007 取模的结果输出。 即输出 P%1000000007
+ * @example: 题目保证输入的数组中没有的相同的数字
+                数据范围：
+            对于%50的数据,   size <= 10^4
+            对于%75的数据,   size <= 10^5
+            对于%100的数据,  size <= 2*10^5
+ * @create 2018-04-05 20:23
+ */
+public class InversePairs {
+
+    public int solutions(int [] array) {
+
+    }
+}
