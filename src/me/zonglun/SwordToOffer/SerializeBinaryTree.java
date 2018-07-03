f8a552ffe84e19496237d525557a230db0949fc9
zonglunLi <mrzonglunli@gmail.com>
2018-07-03 20:16:56 +0800

    Default

---
+++ src/me/zonglun/SwordToOffer/SerializeBinaryTree.java
@@ -0,0 +1,36 @@
+package me.zonglun.SwordToOffer;
+
+import me.zonglun.SwordToOffer.util.TreeNode;
+
+/**
+ * 序列化二叉树
+ *
+ * @author : Administrator
+ * @create 2018-07-03 11:04
+ * @subject :
+ *      请实现两个函数，分别用来序列化和反序列化二叉树
+ */
+public class SerializeBinaryTree {
+
+    /**
+     * 序列化二叉树--将二叉树序列化为字符串
+     *
+     * @param root
+     * @return
+     */
+    String serializeBinary(TreeNode root) {
+        // todo
+        return null;
+    }
+
+    /**
+     * 反序列化二叉树--将字符串转化为二叉树
+     *
+     * @param str
+     * @return
+     */
+    TreeNode Deseriallize(String str) {
+        // todo
+        return null;
+    }
+}
--- src/me/zonglun/SwordToOffer/StringIsNum.java
+++ src/me/zonglun/SwordToOffer/StringIsNum.java
@@ -13,5 +13,6 @@ public class StringIsNum {
         return true;
     }
 
+
     
 }
