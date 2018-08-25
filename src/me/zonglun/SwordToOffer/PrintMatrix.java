a996223963b9f45ea1a7464d9c073d81bbf1be68
ZongLUnli <mrzonglunli@gmail.com>
2018-08-25 21:40:33 +0800

    update offer

--- src/me/zonglun/SwordToOffer/PrintMatrix.java
+++ src/me/zonglun/SwordToOffer/PrintMatrix.java
@@ -11,13 +11,10 @@ import java.util.ArrayList;
  *                              5  6   7     8
  *                              9  10  11   12
  *                              13 14 15    16
- *          则依次打印出数字1,2,3,4, 8,12,16,15,14, 13,9,5,6 ,7,11, 10.
+ *          则依次打印出数字 1,2,3,4, 8,12,16,15,14, 13,9,5,6 ,7,11, 10.
  * @create 2018-03-15 22:01
  */
 public class PrintMatrix {
-
-    // todo 未通过测试案例
-    private ArrayList<Integer> list = new ArrayList<>();
 
     /**
      * @param matrix
@@ -25,44 +22,40 @@ public class PrintMatrix {
      */
     private ArrayList<Integer> printMatrix(int[][] matrix) {
         int rows = matrix.length;
-        int columns = matrix[0].length;
-        int start = 0;
-        while (rows > start * 2 && columns > start * 2) {
-            printMatrixCircle(matrix, rows, columns, start);
-            start++;
+        int cols = matrix[0].length;
+        ArrayList<Integer> list = new ArrayList<>();
+        int start = 0; // start代表层数
+
+        // 对于一个6 * 6的矩阵而言，最后一圈需要打印四个数字
+        // 它的左上角坐标是(2,2)，满足6 > 2 * 2
+        while(2 * start < rows && 2 * start < cols) { // 不能写成 rows/2 的形式
+            int endX = cols - start - 1;
+            int endY = rows - start - 1;
+            // 第一行是肯定要打印的
+            for(int i = start; i <= endX; i++) {
+                list.add(matrix[start][i]);
+            }
+            // 第二步的条件是终止行号大于起始行号
+            if(start < endY) {
+                for(int i = start + 1; i <= endY; i++) {
+                    list.add(matrix[i][endX]);
+                }
+            }
+            // 第三步的条件是至少两行两列，终止行号大于起始行号 并且终止列号大于起始列号
+            if(start < endX && start < endY) {
+                for(int i = endX - 1; i >= start; i--) {
+                    list.add(matrix[endY][i]);
+                }
+            }
+            // 第四步的条件是至少有三行两列，终止行号至少比起始行号大2，终止列号大于起始列号
+            if(start < endX && start < endY - 1) {
+                for(int i = endY - 1; i >= start + 1; i--) {
+                    list.add(matrix[i][start]);
+                }
+            }
+            start++; // 一层一层向里打印
         }
         return list;
     }
 
-    /**
-     * @param matrix
-     * @param rows
-     * @param columns
-     * @param start
-     */
-    private void printMatrixCircle(int[][] matrix, int rows, int columns, int start) {
-        // 从左到右打印一行
-        for (int i = start; i < columns - start; i++) {
-            list.add(matrix[start][i]);
-        }
-        // 从上到下打印一列
-        for (int j = start + 1; j < rows - start; j++) {
-            list.add(matrix[j][columns - start - 1]);
-        }
-        // 从右到左打印一行
-        for (int m = columns - start - 2; m > start && rows - start - 1 > start; m--) {
-            list.add(matrix[rows - start - 1][m]);
-        }
-        // 从下到上打印一列
-        for (int n = rows - start - 2; n >= start + 1 && columns - start - 1 > start; n--) {
-            list.add(matrix[n][start]);
-        }
-    }
-
-
-    public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        PrintMatrix mx = new PrintMatrix();
-        System.out.println(mx.printMatrix(arr));
-    }
 }
